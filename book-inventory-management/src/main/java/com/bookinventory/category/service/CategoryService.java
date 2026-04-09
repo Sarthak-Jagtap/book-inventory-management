ab7f488e0bfd23e021e6e302566b8968bd9a4d5b
sarthak76342 <sarthak22102003@gmail.com>
2026-04-09 14:42:24 +0530

    Category Service Added

---
+++ book-inventory-management/src/main/java/com/bookinventory/category/service/CategoryService.java
@@ -0,0 +1,46 @@
+package com.bookinventory.category.service;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.bookinventory.category.entity.Category;
+import com.bookinventory.category.repository.CategoryRepository;
+
+@Service
+public class CategoryService {
+
+	@Autowired
+	private CategoryRepository categoryRepository;
+	
+	public Category addCategory(Category category) {
+		return categoryRepository.save(category);
+	}
+	
+	public List<Category> getAllCategories(){
+		return categoryRepository.findAll();
+	}
+	
+	public Category getCategoryById(int id) {
+		// Exception Handling Needed
+		return categoryRepository.findById(id).orElse(null);
+	}
+	
+	public Category updateCategory(int id, String newDescription) {
+		// Exception Handling Needed 
+		Category category = categoryRepository.findById(id).orElse(null);
+		
+		if(category != null) {
+			category.setCatDescription(newDescription);
+			return categoryRepository.save(category);
+		}
+		
+		return null;
+	}
+	
+	public void deleteCategory(int id) {
+		// Exception Handling Needed 
+		categoryRepository.deleteById(id);
+	}
+}
