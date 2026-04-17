40c3d156c4ff983d9bc56197f4ac6d5fc3fb1931
sarthak76342 <sarthak22102003@gmail.com>
2026-04-17 14:41:45 +0530

    Created Empty Project for Book Inventory Management FrontEnd.

---
+++ book-inventory-management-frontend/src/test/java/com/bookinventoryfrontend/BookInventoryManagementFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bookinventoryfrontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookInventoryManagementFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
