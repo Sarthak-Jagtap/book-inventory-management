aff2e1269b9a7bb4685e3cb2120a3bae601ae5f0
sarthak76342 <sarthak22102003@gmail.com>
2026-04-19 21:39:29 +0530

    Publisher Request DTO fixed

--- book-inventory-management/src/main/java/com/bookinventory/publisher/dto/PublisherRequestDTO.java
+++ book-inventory-management/src/main/java/com/bookinventory/publisher/dto/PublisherRequestDTO.java
@@ -5,12 +5,23 @@ import jakarta.validation.constraints.NotNull;
 
 public class PublisherRequestDTO {
 
+	@NotNull(message = "Publisher ID is required")
+	private Integer publisherId;
+
 	@NotBlank(message = "Publisher name is required")
 	private String name;
 	private String city;
-	
+
 	@NotNull(message = "State code is required")
 	private String stateCode;
+
+	public Integer getPublisherId() {
+		return publisherId;
+	}
+
+	public void setPublisherId(Integer publisherId) {
+		this.publisherId = publisherId;
+	}
 
 	public String getName() {
 		return name;
